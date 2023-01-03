201f4e0ad6a39e3736132555403baa24d56cf8e6
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-03 05:28:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get("*", function (res, req) {
 });
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://Den4ik_:frgX6V21e7ZugwE7@cluster0.pn5jxqp.mongodb.net/rudenko-art-pro?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://Den4ik_:frgX6V21e7ZugwE7@cluster0.pn5jxqp.mongodb.net/rudenko-art-pro?retryWrites=true&w=majority')
     .then(() => {
         app.listen(port)
     })
