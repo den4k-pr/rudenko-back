04a34c66025120df7086c579fcce14e6f7175b1a
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-03 07:01:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
