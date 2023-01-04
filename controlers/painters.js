374740eee4e587f2d788fc09f0e0a71257d7380f
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-04 02:09:12 +0100

    Update painters.js

--- controlers/painters.js
+++ controlers/painters.js
@@ -70,7 +70,7 @@ const createPainters = async (req, res) => {
         const { city, description, name } = req.body;
 
         const painter = await Painter.create({
-            planeImage: `http://localhost:${process.env.PORT}/static/${req.file.filename}`,
+            planeImage: `https://mern-task-app-sc80.onrender.com/static/${req.file.filename}`,
             city,
             description,
             name
