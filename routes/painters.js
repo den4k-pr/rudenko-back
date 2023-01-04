03287f342f8f49afde11ac6a187894d109377014
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-04 23:46:53 +0100

    Update painters.js

--- routes/painters.js
+++ routes/painters.js
@@ -5,7 +5,7 @@ const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: './build/images/',
+    destination: './assets/',
     filename: (req, file, cb) => {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
