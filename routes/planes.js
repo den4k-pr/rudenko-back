580b4b2ec58507227226af02a75a2a56df5e9f94
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-05 01:19:02 +0100

    Update planes.js

--- routes/planes.js
+++ routes/planes.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const storage = multer.diskStorage({
     destination: './assets/',
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+      cb(null, file.fieldname + '-' + Date.now())
     }
   })
   
