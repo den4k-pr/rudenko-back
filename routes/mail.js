a7aafa41b231effe5dad48ab1d9474d3bd88973f
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-06 19:01:21 +0100

    Delete mail.js

--- routes/mail.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const routes = express.Router();
-
-const {sendMail} = require('../controlers/mail')
-
-routes.post("/sendmail", sendMail);
-module.exports = routes;
