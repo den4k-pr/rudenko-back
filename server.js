bae0a8114087dd63b79926f6219113037cd41b2d
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-06 21:12:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -44,3 +44,30 @@ mongoose.connect('mongodb+srv://Den4ik_:frgX6V21e7ZugwE7@cluster0.pn5jxqp.mongod
 
 app.listen(port)
 
+var nodemailer = require('nodemailer');
+
+var mail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'your-email@gmail.com',
+    pass: 'your-gmail-password'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'yakovden4k@gmail.com',
+  subject: 'Sending Email via Node.js',
+  text: 'That was easy!'
+};
+  
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
+
