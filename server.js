a3bc981aa3011051ef9361b9e89f560b9252a3ea
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-01-06 20:54:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,25 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 const nodemailer = require('nodemailer')
 
+let testEmailAccount = await nodemailer.createTestAccount()
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: testEmailAccount.user,
+    pass: testEmailAccount.pass,
+  },
+})
+
+let result = await transporter.sendMail({
+  from: 'yakovden4k@gmail.com',
+  to: 'yakovden4k@gmail.com',
+  subject: 'yakovden4k@gmail.com',
+  text: 'yakovden4k@gmail.com',
+})
+
+console.log(result)
+
 
 app.use(bodyParser.json());
 app.use(
