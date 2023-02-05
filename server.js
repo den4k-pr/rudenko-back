5c852aadbb4057fb765a681aa752b42d046b0608
den4k-pr <106625230+den4k-pr@users.noreply.github.com>
2023-02-06 00:27:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http:localhost:3000", "https://mern-task-app-sc80.onrender.com"],
+    origin: ["http:localhost:3000", "https://rudenko-art-pro.onrender.com"],
   })
 )
 
